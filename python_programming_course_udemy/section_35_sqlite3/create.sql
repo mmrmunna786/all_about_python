3d29fea6f5c373acc6bdfdda91b2ffb9c4340001
mmrmunna786 <mmrmunna786@gmail.com>
2026-06-20 21:38:28 +0100

    section 35 completed and section 36 has started

---
+++ python_programming_course_udemy/section_35_sqlite3/create.sql
@@ -0,0 +1,22 @@
+CREATE TABLE dogs ( -- creating table called "dogs" with its data members.
+    name TEXT,
+    breed TEXT,
+    age INTEGER
+); -- note the semicolon at the end
+
+CREATE TABLE cats ( -- creating table called "cats" with its data members.
+    name TEXT,
+    breed TEXT,
+    age INTEGER
+); -- note the semicolon at the end
+
+INSERT INTO cats ( -- inserting into "cats" table.
+    name,
+    breed,
+    age
+ ) VALUES ( 
+    "Blue",
+    "Scottish Fold",
+    3
+); -- note the semicolon at the end
+
---
+++ python_programming_course_udemy/section_35_sqlite3/new_entries_in_dog_database.sql
@@ -0,0 +1,5 @@
+-- inseting more dogs entry to the "dog_database.db" database
+INSERT INTO dogs (name, breed, age) VALUES ("Champ", "Husky", 4);
+INSERT INTO dogs (name, breed, age) VALUES ("Rose", "Chihuahua", 11);
+INSERT INTO dogs (name, breed, age) VALUES ("Moose", "Lab", 5);
+INSERT INTO dogs (name, breed, age) VALUES ("Piggy", "Corgi", 9);
